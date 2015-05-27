db7d6f3445b078acaebc133a2c62ca694c1efcd8
hesterj <hesterj@ufl.edu>
2015-05-27 14:53:20 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,9 +5,8 @@
 #include<string.h>
 #include<iostream>
 #include<vector>
+#include <time.h>
 #include "godel.h"
-#include "symbols.h"
-#include "designator.h"
 
 using namespace std;
 
@@ -22,38 +21,89 @@ int main(int argc, char **argv) {
 	VTerm x = VTerm("x");
 	VTerm y = VTerm("y");
 	VTerm z = VTerm("z");
-	Formula a = equals(x,y);
-	Formula b = equals(w,z);
-	Formula c = fExpansion(a,a);
-	Formula temp = fExpansion(c,c);
-	temp = fNegation(temp);
-	Formula f = temp;
-	f = fContraction(f);
-	f = fexIntroduction(f,x);
-	f = fexIntroduction(f,y);
-	f = fExpansion(f,a);
-	f = fexIntroduction(f,x);
-	f = fexIntroduction(f,y);
-	f = fExpansion(f,f);
-	f = fExpansion(f,f);
-	f = fExpansion(f,f);
-	f = fExpansion(f,f);
-	f = fExpansion(f,f);
+	
 
-	string temprep = temp.getRep();
-	string repe = f.getRep();
-	cout << temprep << endl;
-	cout << repe << endl;
-	char *e = new char[repe.length()+1];
-	strcpy(e,repe.c_str());
-	mpz_t t;
-	mpz_init(t);
+	/*  CUT RULE EXAMPLE
+	Formula a = equals(x,x);
+	Formula b = equals(w,z);
+	Formula c = equals(x,z);
+	Formula temp = fNegation(a);
+	temp = fExpansion(temp,c);
+	temp = fNegation(temp);
 	
-	encode(e,&t,repe.length()+1);
-	mpz_out_str(stdout,10,t);
-	puts("");
+	Formula f = fExpansion(a,b);
+	f = fNegation(f);
+	f = fExpansion(f,temp);
+	f = fNegation(f);	
+	f = fCut(f);   */
 	
-	string end = numToString(t);
-	cout << end << endl;
+	 /*  INSTANTIATION RULE EXAMPLE
+	Formula a = equals(x,x);
+	Formula b = equals(w,z);
+	Formula c = equals(x,z);
+	a = fexIntroduction(a,x);
+	b = fNegation(b);
+	Formula f = fExpansion(a,b);
+	f = fDisSwitch(f);  
+	f = fEintroductionRule(f,x);	*/
+	//
 	
+	Formula a = equals(x,x);
+	Formula b = equals(w,z);
+	Formula c = equals(x,z);
+	a = fexIntroduction(a,x);
+	b = fNegation(b);
+	Formula f = fExpansion(a,b);
+	f = fDisSwitch(f);  
+	f = fEintroductionRule(f,x);
+	srand (time(NULL));
+	for (int i=0;i<1000; i++) {
+		int other = 0;
+		int choice = 0;
+		choice = rand()%5;
+		printf("%d\n", choice);
+		if (choice == 0) {
+			other = rand()%3;
+			if (other == 0) {
+				f = fExpansion(f,a);
+			}
+			if (other == 1) {
+				f = fExpansion(f,b);
+			}
+			if (other == 2) {
+				f = fExpansion(f,c);
+			}
+		}
+		if (choice == 1) {
+			f = fAssociative(f);
+		}
+		if (choice == 2) {
+			f = fContraction(f);
+		}
+		if (choice == 3) {
+			f = fEintroductionRule(f,x);
+			f = fEintroductionRule(f,y);
+			f = fEintroductionRule(f,z);
+		}
+		if (choice == 4) {
+			f = fCut(f);
+		}
+		
+		cout << f.getRep() << endl;
+		string repe = f.getRep();
+		//cout << repe << endl;
+		char *e = new char[repe.length()+1];
+		strcpy(e,repe.c_str());
+		mpz_t t;
+		mpz_init(t);
+		
+		encode(e,&t,repe.length()+1);
+		//mpz_out_str(stdout,10,t);
+		puts("");
+		
+		string end = numToString(t);
+		//cout << end << endl;
+	}
+	cout << f.getRep() << endl;
+	return 0;
 }

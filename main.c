40aba69e312618232f0cb0d7587e840e095a74b1
hesterj <hesterj@ufl.edu>
2015-08-13 15:46:40 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include <stack>
 #include<string.h>
 #include<iostream>
 #include<vector>
@@ -11,99 +12,124 @@
 using namespace std;
 
 int main(int argc, char **argv) {
-	if (argv[1] == "-d") {
+	
+	if (strcmp(argv[1],"-encode")==0) {  // string to number
 		
 	}
-	if (argv[1] == "-e") {
+	
+	else if (strcmp(argv[1],"--decode")==0) {  // number to string
 		
 	}
-	VTerm w = VTerm("w");
-	VTerm x = VTerm("x");
-	VTerm y = VTerm("y");
-	VTerm z = VTerm("z");
 	
-
-	/*  CUT RULE EXAMPLE
-	Formula a = equals(x,x);
-	Formula b = equals(w,z);
-	Formula c = equals(x,z);
-	Formula temp = fNegation(a);
-	temp = fExpansion(temp,c);
-	temp = fNegation(temp);
+	else if (strcmp(argv[1],"-prefixtoinfix")==0) {
+		VTerm w = VTerm("w");
+		VTerm x = VTerm("x");
+		VTerm y = VTerm("y");
+		VTerm z = VTerm("z");
+		Formula a = equals(x,x);
+		Formula b = equals(y,y);
+		Formula f = fExpansion(a,b);
+		string result = PrefixToInfix(f);
+		cout << result << endl;
+	}
 	
-	Formula f = fExpansion(a,b);
-	f = fNegation(f);
-	f = fExpansion(f,temp);
-	f = fNegation(f);	
-	f = fCut(f);   */
-	
-	 /*  INSTANTIATION RULE EXAMPLE
-	Formula a = equals(x,x);
-	Formula b = equals(w,z);
-	Formula c = equals(x,z);
-	a = fexIntroduction(a,x);
-	b = fNegation(b);
-	Formula f = fExpansion(a,b);
-	f = fDisSwitch(f);  
-	f = fEintroductionRule(f,x);	*/
-	//
-	
-	Formula a = equals(x,x);
-	Formula b = equals(w,z);
-	Formula c = equals(x,z);
-	a = fexIntroduction(a,x);
-	b = fNegation(b);
-	Formula f = fExpansion(a,b);
-	f = fDisSwitch(f);  
-	f = fEintroductionRule(f,x);
-	srand (time(NULL));
-	for (int i=0;i<1000; i++) {
-		int other = 0;
-		int choice = 0;
-		choice = rand()%5;
-		printf("%d\n", choice);
-		if (choice == 0) {
-			other = rand()%3;
-			if (other == 0) {
-				f = fExpansion(f,a);
-			}
-			if (other == 1) {
-				f = fExpansion(f,b);
-			}
-			if (other == 2) {
-				f = fExpansion(f,c);
-			}
-		}
-		if (choice == 1) {
-			f = fAssociative(f);
-		}
-		if (choice == 2) {
-			f = fContraction(f);
-		}
-		if (choice == 3) {
-			f = fEintroductionRule(f,x);
-			f = fEintroductionRule(f,y);
-			f = fEintroductionRule(f,z);
-		}
-		if (choice == 4) {
-			f = fCut(f);
-		}
+	else if (strcmp(argv[1],"-cut")==0) {  // cut rule examaple
+		VTerm w = VTerm("w");
+		VTerm x = VTerm("x");
+		VTerm y = VTerm("y");
+		VTerm z = VTerm("z");
+		Formula a = equals(x,x);
+		Formula b = equals(w,z);
+		Formula c = equals(x,z);
+		Formula temp = fNegation(a);
+		temp = fExpansion(temp,c);
+		temp = fNegation(temp);
 		
+		Formula f = fExpansion(a,b);
+		f = fNegation(f);
+		f = fExpansion(f,temp);
+		f = fNegation(f);	
+		f = fCut(f);
+	}
+	
+	else if (strcmp(argv[1],"-inst")==0) {  // instantiation rule 
+		VTerm w = VTerm("w");
+		VTerm x = VTerm("x");
+		VTerm y = VTerm("y");
+		VTerm z = VTerm("z");
+		Formula a = equals(x,x);
+		Formula b = equals(w,z);
+		Formula c = equals(x,z);
+		a = fexIntroduction(a,x);
+		b = fNegation(b);
+		Formula f = fExpansion(a,b);
+		f = fDisSwitch(f);  
+		f = fEintroductionRule(f,x);
+	}
+	
+	else if (strcmp(argv[1],"-maze")==0) {  //depth first proof
+		VTerm w = VTerm("w");
+		VTerm x = VTerm("x");
+		VTerm y = VTerm("y");
+		VTerm z = VTerm("z");
+		Formula a = equals(w,x);
+		Formula b = equals(w,z);
+		Formula c = equals(x,z);
+		a = fexIntroduction(a,x);
+		b = fNegation(b);
+		Formula f = fExpansion(a,b);
+		f = fDisSwitch(f);  
+		f = fEintroductionRule(f,x);
+		srand (time(NULL));
+		string first = PrefixToInfix(f);
+		cout << first << endl;
+		for (int i=0;i<40; i++) {
+			int other = 0;
+			int choice = 0;
+			choice = rand()%5;
+			printf("%d\n", choice);
+			if (choice == 0) {
+				other = rand()%3;
+				if (other == 0) {
+					f = fExpansion(f,a);
+				}
+				if (other == 1) {
+					f = fExpansion(f,b);
+				}
+				if (other == 2) {
+					f = fExpansion(f,c);
+				}
+			}
+			if (choice == 1) {
+				f = fAssociative(f);
+			}
+			if (choice == 2) {
+				f = fContraction(f);
+			}
+			if (choice == 3) {
+				f = fEintroductionRule(f,x);
+				f = fEintroductionRule(f,y);
+				f = fEintroductionRule(f,z);
+			}
+			if (choice == 4) {
+				f = fCut(f);
+			}
+			
+			cout << f.getRep() << endl;
+			string repe = f.getRep();
+			char *e = new char[repe.length()+1];
+			strcpy(e,repe.c_str());
+			mpz_t t;
+			mpz_init(t);
+			encode(e,&t,repe.length()+1);
+			puts("");
+			
+			string end = numToString(t);
+		}
 		cout << f.getRep() << endl;
-		string repe = f.getRep();
-		//cout << repe << endl;
-		char *e = new char[repe.length()+1];
-		strcpy(e,repe.c_str());
-		mpz_t t;
-		mpz_init(t);
-		
-		encode(e,&t,repe.length()+1);
-		//mpz_out_str(stdout,10,t);
 		puts("");
-		
-		string end = numToString(t);
-		//cout << end << endl;
+		string result = PrefixToInfix(f);
+		cout << result << endl;
 	}
-	cout << f.getRep() << endl;
 	return 0;
 }
